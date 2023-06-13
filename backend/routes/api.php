9a6c9f4818dcedfc4a4a19e5568298ff859550cb
duarte <gabrieldvr@outlook.com>
2023-06-13 18:39:41 -0300

    conserta ambiente de desenvolvimento / cria controller de teste ✅

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +10,14 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider and are assigned to the
+| "api" middleware group. Enjoy building your API!
 |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Rota corrigida
+Route::get('/teste', [UserController::class, 'status']);
